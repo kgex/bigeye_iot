c63452e4f8e6c6ae2a9f0e42b450fe4bb8f7de1d
Navaneeth Malingan <navneetnivu07@gmail.com>
2022-05-26 21:39:20 +0530

    pickle dump

--- app.py
+++ app.py
@@ -2,41 +2,58 @@ from datetime import datetime
 import requests
 import json
 import time
-d =	{}
+import pickle
 
-url_post = "https://kgxtest.herokuapp.com/attendance_in"
-url_update = "https://kgxtest.herokuapp.com/attendance_out"
+d = {}
 
-headers = {"Content-Type": "application/json; charset=utf-8", "x-hasura-admin-secret" : "4E9fBl6pQoyEL138Ov9jmoY3xnKtMpKm2KtrHWHPOUdcXzMHBzvII9CDooZZH5Ay"}
+# Open the file in binary mode
+with open("backup.pkl", "rb") as file:
+    # Call load method to deserialze
+    d = pickle.load(file)
+    print(d)
+
+url_post = "https://bigbbe.herokuapp.com/attendance_in"
+url_update = "https://bigbbe.herokuapp.com/attendance_out"
+
+headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "x-hasura-admin-secret": "4E9fBl6pQoyEL138Ov9jmoY3xnKtMpKm2KtrHWHPOUdcXzMHBzvII9CDooZZH5Ay",
+}
+
+
+def backup(d):
+    # Open a file and use dump()
+    with open("backup.pkl", "wb") as file:
+        # A new file will be created
+        pickle.dump(d, file)
+
 
 def outentry(id, key):
     print("outentry")
-    now = datetime.now() # current date and time
-    data_out_time = {
-            "id": int(id),
-            "out_time" :now
-        
-    }
+    now = datetime.now()  # current date and time
+    data_out_time = {"id": int(id), "out_time": datetime.timestamp(now)}
     response = requests.patch(url_update, headers=headers, json=data_out_time)
     print(response.json())
     del d[key]
     print(d)
+    backup(d)
+
 
 def inentry(key):
     print("inentry")
-    now = datetime.now() # current date and time
-    data_in_time = {
-        "rfid_key": key,
-        "in_time" :now
-        }
+    now = datetime.now()  # current date and time
+    data_in_time = {"rfid_key": key, "in_time": datetime.timestamp(now)}
     print("here", data_in_time)
     try:
         response = requests.post(url_post, headers=headers, json=data_in_time)
         data = response.json()
-        d[key] = data['insert_attendance_entry']['returning'][0]['id']
+        print(data)
+        d[key] = data["id"]
         print(d)
+        backup(d)
     except Exception as e:
-        print(e)    
+        print(e)
+
 
 def tap(key):
     print("keys", d.keys())
@@ -49,4 +66,3 @@ def tap(key):
 while 1:
     ip = input()
     tap(ip)
-
